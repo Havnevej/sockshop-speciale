ed477f6968ccc37f2cbc31cced2639f9c8cd30b6
Anton D <adu@nemlig.com>
2023-04-14 21:31:20 +0200

    modified the vulnerable python app

--- test/python/server.py
+++ test/python/server.py
@@ -1,26 +1,18 @@
-import subprocess
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+@app.route('/')
+def index():
+    return 'Welcome to my website!'
 
-    # Insecure authentication logic
-    if username == 'admin' and password == 'password':
-        return 'Logged in as admin'
-    else:
-        return 'Invalid username or password'
-
-@app.route('/execute', methods=['POST'])
-def execute():
-    command = request.form.get('command')
-
-    # Insecure command execution
-    result = subprocess.check_output(command, shell=True)
-    return result
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    os.system('ls ./images/' + query)
+    return 'Search results for: ' + query
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
+
