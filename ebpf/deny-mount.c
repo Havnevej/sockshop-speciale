3a0f5e35b53e2f31ba7ef6ad47940faa15c7e682
Anton <antondue8@gmail.com>
2023-04-27 19:58:47 +0200

    added bunch of things that does not work

--- ebpf/deny-mount.c
+++ ebpf/deny-mount.c
@@ -1,27 +1,28 @@
 #include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
 #include <linux/version.h>
 #include <linux/pkt_cls.h>
 
 struct bpf_map_def SEC("maps") denied_cmds = {
     .type = BPF_MAP_TYPE_HASH,
-    .key_size = sizeof(int),
+    .key_size = sizeof(char),
     .value_size = sizeof(int),
     .max_entries = 1024,
 };
 
-SEC("socket")
-int bpf_prog1(struct __sk_buff *skb)
+SEC("kprobe/sys_mount")
+int bpf_prog1(struct pt_regs *ctx)
 {
     char cmd[] = "mount";
     int *val;
 
-    val = bpf_map_lookup_elem(&denied_cmds, &cmd);
+    val = bpf_map_lookup_elem(&denied_cmds, cmd);
     if (val) {
         bpf_printk("Command not allowed: %s\n", cmd);
-        return TC_ACT_SHOT;
+        return -1;
     }
 
-    return TC_ACT_OK;
+    return 0;
 }
 
 char _license[] SEC("license") = "GPL";
---
+++ ebpf/deny-mount.py
@@ -0,0 +1,27 @@
+# Import the necessary BCC modules
+from bcc import BPF
+
+# Define the eBPF program
+prog = """
+#include <uapi/linux/ptrace.h>
+#include <linux/sched.h>
+
+int deny_mount(struct pt_regs *ctx, const char __user *pathname, int flags) {
+    char buf[256];
+    bpf_probe_read_user(buf, sizeof(buf), pathname);
+    if (strstr(buf, "mount") != NULL) {
+        u64 cgroup_id = bpf_get_current_cgroup_id();
+        if (cgroup_id != 0) {
+            return -1;
+        }
+    }
+    return 0;
+}
+"""
+
+
+# Load the eBPF program
+b = BPF(text=prog, debug=4)
+
+# Attach the eBPF program to the mount syscall
+b.attach_kprobe(event="do_mount", fn_name="deny_mount")
