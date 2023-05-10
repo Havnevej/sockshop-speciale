6c3bf3b1ac2beb923bd7be848ecca9e6c42a7802
Anton <antondue8@gmail.com>
2023-05-10 15:40:57 +0200

    Added examples

--- ebpf/deny_mounts.c
+++ ebpf/deny_mounts.c
@@ -6,6 +6,7 @@
 #include <linux/sched.h>
 #include <linux/ns_common.h>
 
+
 /*LSM_PROBE(path_chmod, const struct path *path, umode_t mode)
 {
     bpf_trace_printk("Change mode of file name %s\\n", path->dentry->d_iname);
@@ -19,6 +20,14 @@ struct mnt_namespace {
     atomic_t count;
     struct ns_common ns;
 };
+
+struct cgroup_namespace {
+	struct ns_common	ns;
+	struct user_namespace	*user_ns;
+	struct ucounts		*ucounts;
+	struct css_set          *root_cset;
+};
+
 
 enum event_type {
     EVENT_MOUNT,
@@ -53,36 +62,60 @@ struct data_t {
     };
 };
 
+BPF_PERF_OUTPUT(hey);
+struct data_t2 {
+    u32 pid;
+    char command[16];
+    char message[12];
+    int mount_inum;
+};
+
+static __always_inline void test_func(){
+
+    
+
+}
 
 LSM_PROBE(sb_mount, const char *dev_name, const struct path *path,
 	 const char *type, unsigned long flags, void *dat)
 {
+    if (dev_name == NULL || path == NULL || type == NULL || flags == NULL || dat == NULL) {
+        return false;
+    }
     struct task_struct *task;
     struct data_t event = {};
-
+    struct nsproxy *nsproxy;
+    struct mnt_namespace *mnt_ns;
     task = (struct task_struct *)bpf_get_current_task();
+    if(task){
+        nsproxy = task->nsproxy;
+    }
+    //bpf_trace_printk("pid: %i", *task->pid);
+    if (!nsproxy){
+        return 0;
+    }
+
+    
+    //mnt_ns = nsproxy->mnt_ns;
+    struct data_t2 data = {};
+    char message[12] = "Hello World";
+    data.pid = bpf_get_current_pid_tgid();
+    bpf_get_current_comm(&data.command, sizeof(data.command));
+    bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
+    //data.mount_inum=mnt_ns->ns.inum;
+    hey.perf_submit(ctx, &data, sizeof(data));
+
     char first_7_chars[9];
     bpf_probe_read(first_7_chars, 9, dev_name);
     const char prefix[] = "/dev/sd";
-
     event.pid = bpf_get_current_pid_tgid() & 0xffffffff;
     event.tgid = bpf_get_current_pid_tgid() >> 32;
 
     if (true) {
         // Disallow the mount call
-        bpf_trace_printk("first 7 %s", first_7_chars);
+        //bpf_trace_printk("first 7 %s", first_7_chars);
         //return -EPERM;
     }
-
-
-    struct nsproxy *ns = task->nsproxy;
-    struct ipc_namespace *ipc_ns = ns->ipc_ns;
-    struct mnt_namespace *mnt_ns = ns->mnt_ns;
-    //struct net_namespace *net_ns = ns->net_ns;
-    struct pid_namespace *pid_ns = ns->pid_ns_for_children;
-    //struct user_namespace *user_ns = mnt_ns->user_ns;
-
-    //bpf_printk("ns = %u", mnt_ns->user_ns->level);
 
     bpf_trace_printk("Mount onto path: %s  --- %s \\n", path->dentry->d_iname, dev_name);
     return -1;
--- ebpf/works-primitive.py
+++ ebpf/works-primitive.py
@@ -1,48 +1,6 @@
 from bcc import BPF
-import os
-
-# Define the BPF program
-program = """
-#include <linux/fs.h>
-#include <linux/fs_struct.h>
-#include <linux/nsproxy.h>
-#include <linux/errno.h>
-
-
-#define __LOWER(x) (x & 0xffffffff)
-#define __UPPER(x) (x >> 32)
-LSM_PROBE(path_chmod, const struct path *path, umode_t mode)
-{
-    bpf_trace_printk("Change mode of file name %s\\n", path->dentry->d_iname);
-    return -1;
-}
-LSM_PROBE(sb_mount, const char *dev_name, const struct path *path,
-	 const char *type, unsigned long flags, void *dat)
-{
-    struct task_struct *task;
-    task = (struct task_struct *)bpf_get_current_task();
-
-    char prefix[] = "/dev/sd";
-    //if (__builtin_memcmp(dev_name, prefix, sizeof(prefix) - 1) == 0) {
-    //    // Disallow the mount call
-    //    return -1;
-    //}
-
-
-    struct nsproxy *ns = task->nsproxy;
-    struct ipc_namespace *ipc_ns = ns->ipc_ns;
-    struct mnt_namespace *mnt_ns = ns->mnt_ns;
-    struct net_namespace *net_ns = ns->net_ns;
-    struct pid_namespace *pid_ns = ns->pid_ns_for_children;
-    //struct user_namespace *user_ns = mnt_ns->user_ns;
-
-    
-    bpf_trace_printk("Mount onto path: %s  --- %s \\n", path->dentry->d_iname, dev_name);
-    return -1;
-}
-"""
 
 # Load and attach the BPF program to the 'mount' syscall
-bpf = BPF(src_file="works.c")
+bpf = BPF(src_file="deny_mounts.c")
 
 bpf.trace_print()
