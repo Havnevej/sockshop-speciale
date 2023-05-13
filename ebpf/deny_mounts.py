d42e922edef9dc7372dca9579943a87a44493899
Anton D <adu@nemlig.com>
2023-05-13 22:10:40 +0200

    added comments

--- ebpf/deny_mounts.py
+++ ebpf/deny_mounts.py
@@ -1,14 +1,20 @@
+
+# This code is a bcc program to work alongside deny_mounts.c which denies mount syscalls originating from containers on the system
+
+# Import necessary libraries
 from bcc import BPF
 import os
 from ctypes import c_int, c_uint
 import docker
 import threading
 
-class ContainerMonitorThread(threading.Thread):
+# Define a class for monitoring containers
+class ContainerMonitor(threading.Thread):
     container_pids=[]
     containers=[]
     bpf_obj=[]
 
+    # Define the run method
     def run(self):
         # Load initial list of containers 
         self.container_pids=self.get_container_pids_from_filesystem()
@@ -19,6 +25,7 @@ class ContainerMonitorThread(threading.Thread):
             iter+=1
         client = docker.from_env()
 
+        # Monitor for container events
         for event in client.events(decode=True):
             if event["Type"] == "container" and event["Action"] == "create":
                 self.on_container_create(event)
@@ -40,6 +47,7 @@ class ContainerMonitorThread(threading.Thread):
         print("New container created:", event["Actor"]["ID"])
         self.containers.append(container_id)
 
+    # Get pids from proc file
     def get_pids_from_proc_file(self, container_id):
         container_pids = []
         with open(f'/sys/fs/cgroup/system.slice/docker-{container_id}.scope/cgroup.procs', 'r') as procs_file:
@@ -47,6 +55,7 @@ class ContainerMonitorThread(threading.Thread):
                 container_pids.append(int(pid.strip()))
         return container_pids
     
+    # Get container pids from filesystem
     def get_container_pids_from_filesystem(self):
         container_pids = []
         for file in os.listdir('/sys/fs/cgroup/system.slice/'):
@@ -55,6 +64,7 @@ class ContainerMonitorThread(threading.Thread):
                 container_pids=container_pids+self.get_pids_from_proc_file(container_id)
         return container_pids
 
+    # Update container pids in map
     def update_container_pids_in_map(self, pid):
         iter=0
         for existing_pid in self.bpf_obj["container_pids"]:
@@ -64,13 +74,15 @@ class ContainerMonitorThread(threading.Thread):
                 return
             iter+=1
 
-        # Load and attach the BPF program to the 'mount' syscall
-
 # Create an ebpf map to store container pids
 bpf = BPF(src_file="deny_mounts.c")
 
-monitor_thread = ContainerMonitorThread()
+# Create a container monitor thread
+monitor_thread = ContainerMonitor()
 monitor_thread.bpf_obj=bpf
 monitor_thread.start()
 
+# Print trace
 bpf.trace_print()
+
+
